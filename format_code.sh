16570cf0d839d8f1ed44e293f1dc8964f8ad7ce5
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-06-07 16:14:23 +0300

    run shfmt also in code format script

--- format_code.sh
+++ format_code.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-# Format C++ code using ClangFormat
 set -eo pipefail
 
+# Format C++ code using ClangFormat
+
 REPO=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-clang-format -i -style=file "$REPO"/Source/*.cpp "$REPO"/Source/*.h
+pushd "$REPO" > /dev/null
+
+if [ -z "$(command -v clang-format)" ]; then
+    echo "ERROR: clang-format not found in path"
+    exit 1
+fi
+
+echo "Formatting C++ files..."
+clang-format -i --verbose --style=file "$REPO"/Source/*.cpp "$REPO"/Source/*.h
+
+if [ -n "$(command -v shfmt)" ]; then
+    echo "Formatting shell scripts..."
+    shfmt --list --write ./*.sh
+fi
+
+popd > /dev/null || :
