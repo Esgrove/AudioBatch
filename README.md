44b782eb320dbffd131b26de0d5d8d0e8a5d563e
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-06-07 15:25:21 +0300

    small updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AudioBatch
+
+GUI and CLI app (C++20 & JUCE) for batch processing audio files.
+
+Work in progress...
--- format_code.sh
+++ format_code.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Format C++ code using ClangFormat
+set -eo pipefail
+
 REPO=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 clang-format -i -style=file "$REPO"/Source/*.cpp "$REPO"/Source/*.h
